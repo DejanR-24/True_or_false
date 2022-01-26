3c3317c108e9dc3b0c67283d6417e223e46dad38
Dejan <dejan24dr@gmail.com>
2022-01-26 12:11:51 +0100

    fix small issues url.py and view.py

--- story/urls.py
+++ story/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    path('stories/',views.stories,name='all-stories'),
     path('stories/<int:story_ID>/',views.story_comments,name='story-comments'),
     path('my_stories/',views.my_stories,name='my-stories'),
     path('add_story/',views.add_story,name='add_story'),
-    path("",views.Home.as_view(),name="home"),
     
 ]
--- story/views.py
+++ story/views.py
@@ -6,23 +6,7 @@ from django.views.generic import View
 from .models import Story,Comment
 from .forms import StoryForm,CommentForm
 
-class Home(View):
-    def get(self,request):
-        all_stories =  Story.objects.all() 
-        return render(request, 'story/stories.html',{'stories':all_stories})
 
-class StoryComments(View):
-    def get(self,request,story_ID):
-        story = Story.objects.get(id=story_ID)
-        comments = Comment.objects.filter(story_id=story_ID)
-        username = request.user.username
-        initial_data ={
-        'author': username,
-        }
-        form=CommentForm(request.POST or None,initial=initial_data)
-        if form.is_valid():
-            form.save()
-        return render(request, 'story/comments.html',{'story':story,'comments':comments, 'form':form})
 
 
 def stories(request):
@@ -31,6 +15,7 @@ def stories(request):
 
 def story_comments(request,story_ID):
     comments = Comment.objects.filter(story_id=story_ID)   #story=story.objects.get(id=story_ID)) #SELECT * FROM KOMENTARI WHERE OBJAVE_ID=
+    story = Story.objects.get(id=story_ID)
     username = request.user.username
     initial_data ={
         'story': story_ID,
@@ -39,7 +24,7 @@ def story_comments(request,story_ID):
     form=CommentForm(request.POST or None,initial=initial_data)
     if form.is_valid():
         form.save()
-    return render(request, 'story/comments.html',{'comments':comments, 'form':form})
+    return render(request, 'story/comments.html',{'story':story,'comments':comments, 'form':form})
 
 def my_stories(request):
     username = request.user.username
@@ -55,24 +40,6 @@ def add_story(request):
         return redirect('/my_stories')
     else:
         return render(request, 'story/add_story.html')
-'''
-    form=StoryForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    return render(request, 'story/add_story.html',{'form':form})
-    
 
-    if request.method == 'POST':
-        text= request.POST["text"]
-        ans= request.POST["ans"]
-        author = request.user.username
-        story.objects.create(text = text, ans = ans, author = author).save()
-        '''
 
-class AjaxHandlerView(View):
-    def get(self,request):
-        text = request.GET.get("true_button")
-        print()
-        print(text)
-        print()
-        return render(request, 'story/stories.html')
+
